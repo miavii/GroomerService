efcb84c3f305c0ea3123ed90a2a8e417342cfb6e
mialahtinen <mia.lahtinen@ecraft.com>
2020-11-04 14:06:05 +0200

    Removed excess projects and put some strings away

--- GroomerApp/AzureFunctionStartup.cs
+++ GroomerApp/AzureFunctionStartup.cs
@@ -16,8 +16,7 @@ namespace GroomerApp
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddDbContext<miadatabaseContext>(option =>
-            option.UseSqlServer("Server=tcp:miaco.database.windows.net,1433;Initial Catalog=mia-database;Persist Security Info=False;User ID=magnificent;Password=Jaejoong01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-            );
+            option.UseSqlServer("sqldb_connection"));
         }
     }
 }
